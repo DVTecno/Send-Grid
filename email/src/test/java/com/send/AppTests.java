e8db7eee83074585b204a48e20233fada531732f
Diego Veras <verasdiegoca78@gmail.com>
2025-01-15 21:49:22 -0300

    Rename application and test classes for consistency

--- email/src/test/java/com/send/AppTests.java
+++ email/src/test/java/com/send/AppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SendGridApplicationTests {
+class AppTests {
 
 	@Test
 	void contextLoads() {
