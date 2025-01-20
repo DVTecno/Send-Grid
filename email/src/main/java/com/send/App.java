3fd0835eb716c293f9ed11c64647a6fa897e4d5f
Diego Veras <verasdiegoca78@gmail.com>
2025-01-20 20:41:04 -0300

    Implement SendGrid email functionality and update application properties

--- email/src/main/java/com/send/App.java
+++ email/src/main/java/com/send/App.java
@@ -1,13 +1,33 @@
 package com.send;
 
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(App.class, args);
-	}
+		Email from = new Email(System.getenv("MAIL_USERNAME"));
+		String subject = "Test Email";
+		Email to = new Email("financial.land.sup@gmail.com");
+		Content content = new Content("text/plain", "Hola desde Java");
+		Mail mail = new Mail(from, subject, to, content);
 
+		SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		Request request = new Request();
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+        Response response = sg.api(request);
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getBody());
+        System.out.println(response.getHeaders());
+    }
 }
